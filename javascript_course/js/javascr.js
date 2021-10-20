8b407133de629d27ba6d39618d6166cce060f35e
Ahmad Droobi <55400034+ahmaddroobi99@users.noreply.github.com>
2021-10-20 13:53:23 +0300

    Add files via upload

---
+++ javascript_course/js/javascr.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById("color_button").onmouseleave = function() {
+    document.getElementById("color_button").style['background-color'] = "purple";
+    document.getElementById("color_button").style.transform = "translateX(100px)";
+};
+
